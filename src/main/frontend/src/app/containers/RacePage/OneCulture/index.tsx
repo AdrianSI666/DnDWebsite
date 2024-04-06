07af1e918dac0c5c1091660649356e26306f80e8
Adrian Sledz <79981981+adriansi666@users.noreply.github.com>
2024-04-06 16:50:24 +0200

    Will this show that I made this? v4
    Signed-off-by: AdrianSI666 <adrian9901@tlen.pl>

--- src/main/frontend/src/app/containers/RacePage/OneCulture/index.tsx
+++ src/main/frontend/src/app/containers/RacePage/OneCulture/index.tsx
@@ -35,7 +35,7 @@ const actionDispatch = (dispatch: Dispatch) => ({
 const oneRaceSelect = createSelector(makeSelectOneRace, (raceDTO) => ({
     raceDTO
 }))
-//Test v3
+//Test v4
 export function OneRace(props: IOneRaceProps) {
     let { name } = useParams();
     const [exist, setExist] = useState(false);
