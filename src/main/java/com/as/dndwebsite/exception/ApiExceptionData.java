b49018aeef27d08ede85a8567d0bdcb36f957925
AdrianSI666 <79981981+adriansi666@users.noreply.github.com>
2024-07-08 21:49:24 +0200

    Small fix that occurred during auto fix.

--- src/main/java/com/as/dndwebsite/exception/ApiExceptionData.java
+++ src/main/java/com/as/dndwebsite/exception/ApiExceptionData.java
@@ -1,11 +1,8 @@
 package com.as.dndwebsite.exception;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-@Getter
 public record ApiExceptionData(String message, HttpStatus httpStatus, ZonedDateTime timeStamp) {
-
 }
